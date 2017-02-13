aed5fdd0b73d01a597b27551da90339e448dd39d
Piers Holt <piersholt@gmail.com>
2017-02-13 13:19:21 +1100

    Refactor

--- src/search/components/search.jsx
+++ src/search/components/search.jsx
@@ -8,28 +8,26 @@ import { FilterContainer } from '../containers/filter'
 
 class Search extends Component {
   render() {
-    return (
-      <div className="Search">
-        <div className="row">
-          { /** Search Filter + Results **/ }
-          <div className="col-md-9" id="SearchFilterResults">
-            { /** Search Filter **/ }
-            <div className="row">
-              <div className="col-md-12" id="SearchFilter">
-                <FilterContainer />
-              </div>
-            </div>
-            { /** Search Results **/ }
-            <div className="row">
-              <div className="col-md-12" id="SearchResults">
-                <ResultsContainer accounts={{}}/>
-              </div>
+    return (    
+      <div className="row">
+        { /** Search Filter + Results **/ }
+        <div className="col-md-9" id="SearchFilterResults">
+          { /** Search Filter **/ }
+          <div className="row">
+            <div className="col-md-12" id="SearchFilter">
+              <FilterContainer />
             </div>
           </div>
-          { /** Selected Profile **/ }
-          <div className="col-md-3" id="SearchProfile">
-            <ProfileContainer />
+          { /** Search Results **/ }
+          <div className="row">
+            <div className="col-md-12" id="SearchResults">
+              <ResultsContainer accounts={{}}/>
+            </div>
           </div>
+        </div>
+        { /** Selected Profile **/ }
+        <div className="col-md-3" id="SearchProfile">
+          <ProfileContainer />
         </div>
       </div>
     );
