95f1d0d23bb062aeabc96f9e8061fde9bc77140c
Piers Holt <piersholt@gmail.com>
2017-02-13 11:49:28 +1100

    Move Search to own namespace

--- src/App.js
+++ src/App.js
@@ -7,38 +7,13 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 // Components
-import Filter from './search/components/filter';
-
-// Containers
-import { ResultsContainer } from './search/containers/results'
-import { ProfileContainer } from './search/containers/profile'
-import { FilterContainer } from './search/containers/filter'
+import Search from './search/components/search';
 
 class App extends Component {
   render() {
     return (
       <div className="BaxterApp">
-        <div className="row">
-          { /** Search Filter + Results **/ }
-          <div className="col-md-9" id="SearchFilterResults">
-            { /** Search Filter **/ }
-            <div className="row">
-              <div className="col-md-12" id="SearchFilter">
-                <FilterContainer />
-              </div>
-            </div>
-            { /** Search Results **/ }
-            <div className="row">
-              <div className="col-md-12" id="SearchResults">
-                <ResultsContainer accounts={{}}/>
-              </div>
-            </div>
-          </div>
-          { /** Selected Profile **/ }
-          <div className="col-md-3" id="SearchProfile">
-            <ProfileContainer />
-          </div>
-        </div>
+        <Search />
       </div>
     );
   }
