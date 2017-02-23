48d330605c89863e10fc344419fd2482321ad976
Piers Holt <piersholt@gmail.com>
2017-02-23 22:02:14 +1100

    Make filter, overview, and results show dynamically

--- src/search/components/search.jsx
+++ src/search/components/search.jsx
@@ -8,41 +8,6 @@ import { FilterContainer } from '../containers/filter'
 import { OverviewContainer } from '../containers/overview'
 
 class Search extends Component {
-  showResults() {
-    if (true === true) {
-      return (
-
-        <div className="container">
-          <div className="row">
-
-            <div className="col-md-9" id="SearchFilterResults">
-              { this.showFilter() }
-
-              { /** Search Overview **/ }
-              <div className="row">
-                <div className="col-md-12" id="SearchOverview">
-                  <OverviewContainer />
-                </div>
-              </div>
-
-              { /** Search Results **/ }
-              <div className="row">
-                <div className="col-md-12" id="SearchResults">
-                  <ResultsContainer accounts={{}}/>
-                </div>
-              </div>
-
-            </div>
-            { /** Selected Profile **/ }
-            <div className="col-md-3" id="SearchProfile">
-              <ProfileContainer />
-            </div>
-          </div>
-        </div>
-      )
-    }
-  }
-
   showFilter() {
     if (this.props.showFilter === true) {
       return (
@@ -52,10 +17,50 @@ class Search extends Component {
     }
   }
 
+  showOverview() {
+    if (this.props.showOverview === true) {
+      return (
+        /** Search Overview **/
+        <div className="row">
+          <div className="col-md-12" id="SearchOverview">
+            <OverviewContainer />
+          </div>
+        </div>
+      )
+    }
+  }
+
+  showResults() {
+    if (this.props.showResults === true) {
+      return (
+        /** Search Results **/
+        <div className="row">
+          <div className="col-md-12" id="SearchResults">
+            <ResultsContainer accounts={{}}/>
+          </div>
+        </div>
+
+      )
+    }
+  }
+
   render() {
     return (
       <div id="Search">
-        { this.showResults() }
+        <div className="container">
+          <div className="row">
+
+            <div className="col-md-9" id="SearchFilterResults">
+              { this.showFilter() }
+              { this.showOverview() }
+              { this.showResults() }
+            </div>
+            { /** Selected Profile **/ }
+            <div className="col-md-3" id="SearchProfile">
+              <ProfileContainer />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/search/containers/search.jsx
+++ src/search/containers/search.jsx
@@ -3,13 +3,23 @@ import Search from '../components/search'
 
 let mapStateToProps = (state) => {
   let showResults = false
+  let showFilter = state.showFilter
+  let showOverview = !showFilter
+
   if (state.accounts.length > 0) {
     showResults = true
   }
 
+  if (state.accounts.length == 0) {
+    showFilter = true
+    showOverview = false
+    showResults = false
+  }
+
   return {
     showResults: showResults,
-    showFilter: state.showFilter
+    showFilter: showFilter,
+    showOverview: showOverview
   }
 };
 
