39129e3da203a7c7baba5322d9916b3826c2b0b6
Piers Holt <piersholt@gmail.com>
2017-02-24 12:28:18 +1100

    Remove the need for pagination to pass parameters- they are now loaded from state.

--- src/search/components/results.jsx
+++ src/search/components/results.jsx
@@ -4,30 +4,7 @@ import { ResultContainer } from '../containers/result.jsx';
 class Results extends React.Component {
   showPaginate() {
     if (this.props.showPaginate === true) {
-      return <button className="btn-rounded pull-right" onClick={() => this.props.paginateSearch(this.parameters())} type="button">Load More</button>
-    }
-  }
-
-  parameters() {
-    return {
-      segment: {
-        fashion: (this.props.search_parameters.segment.fashion),
-        beauty: (this.props.search_parameters.segment.beauty),
-        lifestyle: (this.props.search_parameters.segment.lifestyle),
-        food: (this.props.search_parameters.segment.food),
-        health: (this.props.search_parameters.segment.health),
-        fitness: (this.props.search_parameters.segment.fitness),
-      },
-      origin: {
-        VIC: (this.props.search_parameters.origin.VIC),
-        NSW: (this.props.search_parameters.origin.NSW),
-        QLD: (this.props.search_parameters.origin.QLD),
-        SA:(this.props.search_parameters.origin.SA),
-        WA:(this.props.search_parameters.origin.WA)
-      },
-      minFollowers: (this.props.search_parameters.minFollowers),
-      maxFollowers: (this.props.search_parameters.maxFollowers),
-      page: (this.props.search_parameters.page)
+      return <button className="btn-rounded pull-right" onClick={() => this.props.paginateSearch(this.props.search_parameters)} type="button">Load More</button>
     }
   }
 
