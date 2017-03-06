ed2f8a82004dcd4679313b22c3d33c0ca236c0dc
Piers Holt <piersholt@gmail.com>
2017-03-06 13:48:35 +1100

    Add followers to search overview

--- src/search/components/overview.jsx
+++ src/search/components/overview.jsx
@@ -4,7 +4,9 @@ class Overview extends React.Component {
   render () {
     return (
       <div>
-        <div className="search-overview pull-left">{ this.props.origin }&nbsp;&nbsp;|&nbsp;&nbsp;{ this.props.segments }</div>
+        <div className="search-overview pull-left">
+          { this.props.origin }&nbsp;&nbsp;|&nbsp;&nbsp;{ this.props.segments }&nbsp;&nbsp;|&nbsp;&nbsp;{ this.props.followers }
+        </div>
         <button className="btn-outline pull-right" onClick={() => this.props.showFilter(true)} type="button">Show Search</button>
       </div>
 
--- src/search/containers/overview.jsx
+++ src/search/containers/overview.jsx
@@ -3,6 +3,8 @@ import Overview from '../components/overview'
 import { showFilter } from '../actions'
 import { buildString } from './profile'
 
+import numeral from 'numeral';
+
 let mapStateToProps = (state) => {
   let segmentOutput = []
   let searchParametersSegments = state.search_parameters.segment
@@ -30,9 +32,15 @@ let mapStateToProps = (state) => {
   originOutput = buildString(originOutput)
 
 
+  let minFollowersFormatted = numeral(state.search_parameters.minFollowers).format('0a')
+  let maxFollowersFormatted = numeral(state.search_parameters.maxFollowers).format('0a')
+
+  let followersOutput = minFollowersFormatted + ' to ' + maxFollowersFormatted + ' followers'
+
   return {
     segments: segmentOutput,
-    origin: originOutput
+    origin: originOutput,
+    followers: followersOutput
   }
 };
 
