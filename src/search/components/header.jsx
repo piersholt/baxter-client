c5e168834d7f4380f0a10e30bf1bf6e4875f462d
Piers Holt <piersholt@gmail.com>
2017-03-17 22:09:35 +1100

    Update nav brand icon to use react-router Link component

--- src/search/components/header.jsx
+++ src/search/components/header.jsx
@@ -1,5 +1,6 @@
 // Core
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Header extends Component {
   render() {
@@ -7,7 +8,7 @@ class Header extends Component {
       <header className="navbar navbar-inverse navbar-fixed-top">
         <div className="container-fluid">
           <div className="navbar-header">
-            <a className="navbar-brand" href="#"><img src={require('../../../public/logo-baxter@2x.png')}/></a>
+            <Link className="navbar-brand" to="/"><img src={require('../../../public/logo-baxter@2x.png')}/></Link>
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
